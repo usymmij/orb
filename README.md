93812c266feb37124faec2b761354c53d26caf60
usymmij <usymmij@gmail.com>
2025-08-22 17:34:27 -0400

    Wavefunction struct, reuse polynomials

--- README.md
+++ README.md
@@ -4,14 +4,16 @@ A visualizer of hydrogen's atomic orbitals in different states
 
 - Written with rust and wgpu
 
-## note on hosting
-- note that WebGPU requires https to work, a debug server on http will cause browsers to throw rendering errors
+## note on self hosting
+- note that WebGPU requires https to work, so testing on a non localhost server 
+without http will cause browsers to throw rendering errors
 
 
 ## References
 
 | Reference                                                                                                                                                                                                         | Author                       |
 |--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|------------------------------|
+| [Original Inspiration](https://www.youtube.com/watch?v=W2Xb2GFK2yc&ab_channel=minutephysics)                                                                                                                      | Henry Reich (MinutePhysics)  |
 | [Learn Wgpu](https://sotrh.github.io/learn-wgpu/)                                                                                                                                                                 | Benjamin Hansen              |
 | [Quantum Mechanics with Python: Hydrogen Wavefunctions and Electron Density Plots](https://ssebastianmag.medium.com/computational-physics-with-python-hydrogen-wavefunctions-electron-density-plots-8fede44b7b12) | Sebastian Mag                |
 | [Ray Tracing in One Weekend](https://github.com/RayTracing/raytracing.github.io?tab=readme-ov-file)                                                                                                               | Shirley, Black, and Hollasch |
--- src/wavefunction.rs
+++ src/wavefunction.rs
@@ -1,7 +1,15 @@
-use std::f64::consts::PI;
-
 use num::complex::ComplexFloat;
 use scilib::math::polynomial::Poly;
+use std::f64::consts::PI;
+
+struct Wavefunction {
+    n: i32,
+    l: i32,
+    m: i32,
+    a0: f64,
+    laguerre_poly: Poly,
+    legendre_poly: Poly,
+}
 
 fn factorial(n: i32) -> i32 {
     if n == 0 {
@@ -10,84 +18,98 @@ fn factorial(n: i32) -> i32 {
     return factorial(n - 1) * n;
 }
 
-fn radial(n: i32, l: i32, r: f64, a0: f64) -> f64 {
-    let n_f: f64 = n as f64;
-    let l_f: f64 = l as f64;
-    let p: f64 = 2.0 * r / (n_f * a0);
+impl Wavefunction {
+    pub fn new(n: i32, l: i32, m: i32, a0: f64) -> Wavefunction {
+        let n_f = n as f64;
+        let lag_coef = f64::sqrt(
+            ((2.0 / n_f * a0).powi(3) * (factorial(n - l - 1) as f64))
+                / (2.0 * n_f * (factorial(n + l) as f64)),
+        );
+        let lag = Poly::laguerre((n - l - 1).try_into().unwrap(), 2.0 * l as f32 + 1.0) * lag_coef;
+        let neg = if m % 2 == 0 { 1.0 } else { -1.0 };
+        let leg_coef = (neg)
+            * (((2 * l + 1) * factorial(l - (m.abs()))) as f64
+                / (4.0 * PI * factorial(l + m.abs()) as f64))
+                .sqrt();
+        let leg = Poly::gen_legendre(l.try_into().unwrap(), m.try_into().unwrap()) * leg_coef;
 
-    let k = f64::sqrt(
-        ((2.0 / n_f * a0).powi(3) * (factorial(n - l - 1) as f64))
-            / (2.0 * n_f * (factorial(n + l) as f64)),
-    );
+        Wavefunction {
+            n: n,
+            l: l,
+            m: m,
+            a0: a0,
+            laguerre_poly: lag,
+            legendre_poly: leg,
+        }
+    }
 
-    let lag = Poly::laguerre((n - l - 1).try_into().unwrap(), 2.0 * l_f + 1.0);
+    fn radial(&self, r: f64) -> f64 {
+        let n_f: f64 = self.n as f64;
 
-    return lag.compute(p) * (k * (-p / 2.0).exp() * (p.powf(l_f)));
+        let p: f64 = 2.0 * r / (n_f * self.a0);
+        return self.laguerre_poly.compute(p) * (-p / 2.0).exp() * (p.powi(self.l));
+    }
+    fn angular(&self, theta: f64, phi: f64) -> f64 {
+        let complex_angle = num::complex::Complex::new(0.0, self.m as f64 * phi).exp();
+        return self.legendre_poly.compute(theta.cos()) * complex_angle.re();
+    }
 }
 
-fn angular(l: i32, m: i32, theta: f64, phi: f64) -> f64 {
-    let leg_poly: f64 =
-        Poly::gen_legendre(l.try_into().unwrap(), m.try_into().unwrap()).compute(theta.cos());
-    let neg: f64 = if m % 2 == 0 { 1.0 } else { -1.0 };
-    let k = (neg)
-        * (((2 * l + 1) * factorial(l - (m.abs()))) as f64
-            / (4.0 * PI * factorial(l + m.abs()) as f64))
-            .sqrt();
-
-    let complex_angle = num::complex::Complex::new(0.0, m as f64 * phi).exp();
-    return k * leg_poly * complex_angle.re();
-}
-
-pub fn sample_wavefunction_prob(
-    n: i32,
-    l: i32,
-    m: i32,
-    a0: f64,
-    r: f64,
-    theta: f64,
-    phi: f64,
-) -> f64 {
-    let rad = radial(n, l, r, a0);
-    let ang = angular(m, l, theta, phi);
-
-    let psi = rad * ang;
-    return psi.powi(2);
-}
+//pub fn sample_wavefunction_prob(
+//    n: i32,
+//    l: i32,
+//    m: i32,
+//    a0: f64,
+//    r: f64,
+//    theta: f64,
+//    phi: f64,
+//) -> f64 {
+//    let rad = radial(n, l, r, a0);
+//    let ang = angular(m, l, theta, phi);
+//
+//    let psi = rad * ang;
+//    return psi.powi(2);
+//}
 
 #[cfg(test)]
 mod test {
-    use super::angular;
+    use super::Wavefunction;
     use super::factorial;
-    use super::radial;
-
-    // unit tests for math I don't understand
 
     #[test]
-    fn test_norm_radial() {
-        // factorial
+    fn test_facorials() {
         let ans: [i32; 5] = [1, 1, 2, 24, 40320];
         let input: [i32; 5] = [0, 1, 2, 4, 8];
         for i in 1..5 {
             assert_eq!(factorial(input[i]), ans[i]);
         }
+    }
 
+    #[test]
+    fn test_norm_radial() {
         // norm radial and laguerre stuff
-        let mut diff: f64 = radial(2, 1, 1.0, 1.0) - 0.1238;
+
+        let atom = Wavefunction::new(2, 1, 1, 1.0);
+        let diff: f64 = atom.radial(1.0) - 0.1238;
         assert_eq!(diff < 0.00001, diff > -0.00001);
 
-        diff = radial(5, 1, 2.0, 3.0) - 0.14356;
+        let atom = Wavefunction::new(5, 1, 1, 3.0);
+        let diff: f64 = atom.radial(2.0) - 0.14356;
         assert_eq!(diff < 0.00001, diff > -0.00001);
 
-        diff = radial(5, 3, 2.0, 1.0) - 0.000984;
+        let atom = Wavefunction::new(5, 3, 1, 1.0);
+        let diff: f64 = atom.radial(2.0) - 0.000984;
         assert_eq!(diff < 0.00001, diff > -0.00001);
     }
 
     #[test]
     fn test_angular() {
-        let mut diff: f64 = angular(3, 1, 0.21, 2.0) + 0.10605;
+        let atom = Wavefunction::new(4, 3, 1, 1.0);
+        let diff: f64 = atom.angular(0.21, 2.0) + 0.10605;
         assert_eq!(diff < 0.001, diff > -0.001);
 
-        diff = angular(4, 1, 2.0, 3.0) + 0.316835;
+        let atom = Wavefunction::new(5, 4, 1, 1.0);
+        let diff: f64 = atom.angular(2.0, 3.0) + 0.316835;
         assert_eq!(diff < 0.001, diff > -0.001);
     }
 }
